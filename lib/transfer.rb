d3a3edcb21294d927abfea1be07814d24dcfea5f
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-07-29 19:33:32 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -25,7 +25,7 @@ end
       @status = "complete"
 else
      return      status = "rejected"
-     "Transaction rejected. Please check your account balance."
+     return "Transaction rejected. Please check your account balance."
 
 end
 end
