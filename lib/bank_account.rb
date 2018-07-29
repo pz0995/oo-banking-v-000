e62b93df12e30642b82601cc12a8e3d6e50ecccf
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-07-29 11:22:28 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,35 @@
 class BankAccount
 
+  attr_accessor :name, :status, :validity, :balance
+
+  def initialize(name, balance, status)
+    bank_account.name =  bank_account.new = 1000.freeze
+    # @balance = 1000
+    status = "open".freeze
+  end
+
+# name, balance, status needs to be frozen
+  def deposit
+    puts bank_account = @balance + deposit
+  end
+
+  def display_balance
+      @balance = "Your balance is $#{balance}."
+  end
+
+  def valid?
+   if bank_account == "open" && @balance > 0
+     return true
+   else
+     return false
+     end
+   end
+
+  def close_account
+    if bank_account_closed == true
+      status = "closed"
+    else
+      status = "open"
+    end
+  end
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,53 @@
 class Transfer
-  # your code here
+
+attr_accessor :transfer, :bad_transfer, :amount
+
+  def initialize
+    @transfer = transfer.new
+    sender = sender_name
+    receiver = receiver_name
+    amount = transfer_amount
+    transfer = transfer.count
+    status = "pending".freeze
+  end
+#status freeze
+
+  def valid?
+    if sender. == valid && receiver == valid
+      return true
+    else
+      return false
+    end
+  end
+
+def execute_transaction
+  if amount != 0
+  sender - deposit && receiver + deposit
+  return true
+  end
 end
+
+def reverse_transfer
+  if execute_transaction == true
+  receiver - deposit && sender + deposit
+  end
+end
+
+end
+  #
+  # #    initialize
+  #     can initialize a Transfer (FAILED - 9)
+  #     initializes with a sender (FAILED - 10)
+  #     initializes with a receiver (FAILED - 11)
+  #     always initializes with a status of 'pending' (FAILED - 12)
+  #     initializes with a transfer amount (FAILED - 13)
+  #   #valid?
+  #     can check that both accounts are valid (FAILED - 14)
+  #     calls on the sender and reciever's #valid? methods (FAILED - 15)
+  #   #execute_transaction
+  #     can execute a successful transaction between two accounts (FAILED - 16)
+  #     each transfer can only happen once (FAILED - 17)
+  #     rejects a transfer if the sender doesn't have a valid account (FAILED - 18)
+  #   #reverse_transfer
+  #     can reverse a transfer between two accounts (FAILED - 19)
+  #     it can only reverse executed transfers (FAILED - 20)
