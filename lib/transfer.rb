90a26ce0493ace1bf03b2425f8b16c64db06dcf3
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-07-29 17:59:05 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -27,7 +27,7 @@ rejected
   end
 
   def reverse_transfer
-    if valid? && @receiver.balance > @amount && status == "complete"
+    if status == "complete" && @receiver.balance > @amount && valid? 
       @receiver.balance  -= amount
       @sender.balance  += amount
       @status  = "reversed"
