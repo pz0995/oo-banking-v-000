b32fd13cc4540fc210caaaebb7b13bcd8a42ce73
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-07-29 14:42:51 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -3,7 +3,7 @@ class Transfer
 attr_accessor :sender, :receiver, :bad_transfer, :transfer_amount, :transfer_status
 attr_reader :status
 
-  def initialize(sender, receiver, status = "pending")
+  def initialize(sender, receiver, status = "pending", transfer_amount)
     @status = status
     @sender = sender
     @receiver = receiver
@@ -35,20 +35,3 @@ end
   end
 end
 end
-  #
-  # #    initialize
-  #     can initialize a Transfer (FAILED - 9)
-  #     initializes with a sender (FAILED - 10)
-  #     initializes with a receiver (FAILED - 11)
-  #     always initializes with a status of 'pending' (FAILED - 12)
-  #     initializes with a transfer amount (FAILED - 13)
-  #   #valid?
-  #     can check that both accounts are valid (FAILED - 14)
-  #     calls on the sender and reciever's #valid? methods (FAILED - 15)
-  #   #execute_transaction
-  #     can execute a successful transaction between two accounts (FAILED - 16)
-  #     each transfer can only happen once (FAILED - 17)
-  #     rejects a transfer if the sender doesn't have a valid account (FAILED - 18)
-  #   #reverse_transfer
-  #     can reverse a transfer between two accounts (FAILED - 19)
-  #     it can only reverse executed transfers (FAILED - 20)
