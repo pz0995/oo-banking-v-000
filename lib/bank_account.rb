39d0085029229ee7bdeff3af88192d63919c500c
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-07-29 11:42:42 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -3,11 +3,11 @@ class BankAccount
   attr_accessor :name, :status, :validity, :balance, :bank_account
 
   def initialize(name, balance, status)
-    @name =  @name.freeze
+    @name =  name.freeze
     @balance = 1000.freeze
     @balance = balance.freeze
     @status = "open"
-    @status = @status.freeze
+    @status = status.freeze
   end
 
 # name, balance, status needs to be frozen
