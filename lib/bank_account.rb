a3996f8ab168a4628b412b5f10b088a30b4811e8
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-07-29 17:32:27 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -28,8 +28,6 @@ class BankAccount
 
 end
 
-# end
-# can't change its name (FAILED - 1)
-#   #deposit
+
 #     can deposit money into its account (FAILED - 2)
-#        can close its account (FAILED - 3)
+#      is valid with an open status and a balance greater than 0
