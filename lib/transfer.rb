bef5976283126f990c30e14123e8272b9898e618
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-07-29 19:09:30 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -25,8 +25,8 @@ end
       @receiver.balance  += amount
       @status = "complete"
     else
-    else
-      !(sender.valid? && receiver.valid?) && @status == "rejected"    
+
+      !(sender.valid? && receiver.valid?) && @status == "rejected"
         return reject_transfer
     end
 end
