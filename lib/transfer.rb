70d9647b7456a2d5572a7ec17e24d1a26d392af4
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-07-29 15:50:54 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,7 +20,7 @@ attr_reader :status
   end
 
   def execute_transaction
-    if valid? && @sender.balance > @amount && status == "pending"
+    if valid? && @sender.balance > @amount
       @sender.balance  -= amount
       @receiver.balance  += amount
       status = "complete"
