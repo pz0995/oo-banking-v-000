e1dc746d959cfc4197f4b5401aaa8f856fba6477
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-07-29 11:57:36 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -5,7 +5,7 @@ class BankAccount
   def initialize(name, balance = 1000, status = "open")
     @name = name
     @name =  name.freeze
-    # @balance = balance.freeze
+    @balance = balance.freeze
     @status = "open"
     # @status = status.freeze
   end
