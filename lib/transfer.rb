e2375405d1cbcde39b998b0599d48bc6eaa1f7c9
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-07-29 14:02:43 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -6,7 +6,7 @@ attr_accessor :transfer, :sender, :receiver, :bad_transfer, :status, :transfer_a
     @name = name
     @sender = sender
     @receiver = receiver
-    @status = "pending"
+    @status = status
     @transfer_amount = transfer_amount
   end
 
